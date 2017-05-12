579ae0f0454ccdd1e2bd9cae620b756355e911e8
saileshkasaju <leosailesh@gmail.com>
2017-05-12 22:19:22 +0545

    move code to app and use react ref to get value ot input field

--- src/App.js
+++ src/App.js
@@ -2,7 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Card = (props) => (
+    <div style={{margin: '1em'}}>
+    <img width="75" alt="card Profile Pic" src={props.avatar_url} />
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+        <div>{props.company}</div>
+      </div>
+    </div>
+);
+
+const CardList = (props) => (
+  <div>
+    {props.cards.map((card, index) => (
+      <Card {...card}
+        key={index}
+        />
+      ))}
+      
+  </div>
+);
+  
+class Form extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Event: FormSubmit', this.userNameInput.value);
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text"
+          ref={(input) => this.userNameInput = input}
+          placeholder="Github username"
+          required />
+        <button type="submit">Add card</button>
+      </form>
+    );
+  }
+}
+
 class App extends Component {
+  state = {
+    cards: [{
+        name: "Paul O'Shannessy",
+        avatar_url: "https://avatars2.githubusercontent.com/u/8445?v=3",
+        company: "Facebook"
+      }, {
+        name: "Ben Alpert",
+        avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=3",
+        company: "Facebook"
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
@@ -10,9 +61,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form/>
+        <CardList cards={this.state.cards}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
 
-const Card = (props) => (
-	  <div style={{margin: '1em'}}>
-    <img width="75" alt="card Profile Pic" src={props.avatar_url} />
-	    <div style={{display: 'inline-block', marginLeft: 10}}>
-	      <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
-	      <div>{props.company}</div>
-	    </div>
-	  </div>
-);
-
-let data = [
-  {
-  	name: "Paul O'Shannessy",
-  	avatar_url: "https://avatars2.githubusercontent.com/u/8445?v=3",
-  	company: "Facebook"
-  },
-  {
-  	name: "Ben Alpert",
-  	avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=3",
-  	company: "Facebook"
-  }
-];
-
-const CardList = (props) => (
-	<div>
-	  {props.cards.map(card => (
-	  	<Card {...card}
-  	    />
-	  	))}
-  	  
-	</div>
-);
-	
 
 ReactDOM.render(
-  <CardList cards={data}/>,
+  <App/>,
   document.getElementById('root')
 );
