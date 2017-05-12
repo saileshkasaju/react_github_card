ddbb102aaa3fc7f998ed27bbcb8de3e62adfd060
saileshkasaju <leosailesh@gmail.com>
2017-05-12 21:55:16 +0545

    add card Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 
+const Card = (props) => (
+	  <div style={{margin: '1em'}}>
+    <img width="75" alt="card Profile Pic" src={props.avatar_url} />
+	    <div style={{display: 'inline-block', marginLeft: 10}}>
+	      <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+	      <div>{props.company}</div>
+	    </div>
+	  </div>
+);
+
+const CardList = (props) => (
+	<div>
+  	  <Card name="Paul O'Shannessy"
+  	    avatar_url="https://avatars2.githubusercontent.com/u/8445?v=3"
+  	    company="facebook"
+  	    />
+	</div>
+);
+	
+
 ReactDOM.render(
-  <App />,
+  <CardList/>,
   document.getElementById('root')
 );
